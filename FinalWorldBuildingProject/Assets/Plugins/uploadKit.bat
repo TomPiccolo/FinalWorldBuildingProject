7a066cbc669b97555bcb145be211544ef139c60d
TomPiccolo <50777275+TomPiccolo@users.noreply.github.com>
2020-12-27 12:28:02 -0500

    DeletedSomeAssetsInScenenProject

--- FinalWorldBuildingProject/Assets/Plugins/uploadKit.bat
+++ FinalWorldBuildingProject/Assets/Plugins/uploadKit.bat
@@ -1,1 +1,1 @@
-curl -v -b cookie -X PUT -F "kit[zip]=@/Users/thomaspiccolo/Documents/GitHub/FinalWorldBuildingProject/FinalWorldBuildingProject/1634311459729047552_checkifpressed.zip" -F "kit[game_engine_version]=20194" https://account.altvr.com/api/kits/1634311459729047552.json
+curl -v -b cookie -X PUT -F "kit[zip]=@/Users/thomaspiccolo/Documents/GitHub/FinalWorldBuildingProject/FinalWorldBuildingProject/1634345334891086493_checkifpressed.zip" -F "kit[game_engine_version]=20194" https://account.altvr.com/api/kits/1634345334891086493.json
