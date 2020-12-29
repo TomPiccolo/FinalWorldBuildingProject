1dd864b407fc43ee6bc5694f840c625f7ccccc62
TomPiccolo <50777275+TomPiccolo@users.noreply.github.com>
2020-12-29 08:20:42 -0500

    ReAnimatedHandinProperRotation
    Corrected Hand animation in proper rotation

--- FinalWorldBuildingProject/Assets/Plugins/uploadKit.bat
+++ FinalWorldBuildingProject/Assets/Plugins/uploadKit.bat
@@ -1,1 +1,1 @@
-curl -v -b cookie -X PUT -F "kit[zip]=@/Users/thomaspiccolo/Documents/GitHub/FinalWorldBuildingProject/FinalWorldBuildingProject/1634345334891086493_checkifpressed.zip" -F "kit[game_engine_version]=20194" https://account.altvr.com/api/kits/1634345334891086493.json
+curl -v -b cookie -X PUT -F "kit[zip]=@/Users/thomaspiccolo/Documents/GitHub/FinalWorldBuildingProject/FinalWorldBuildingProject/1637838345809691190_checkpressedupload.zip" -F "kit[game_engine_version]=20194" https://account.altvr.com/api/kits/1637838345809691190.json
