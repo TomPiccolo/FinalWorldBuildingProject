895802852cb76a5fad9b240f9726375dc21eaa5b
TomPiccolo <50777275+TomPiccolo@users.noreply.github.com>
2020-12-29 08:54:24 -0500

    SucessBuildHandZaxisoff
    Hand not on code on z axis

--- FinalWorldBuildingProject/Assets/Plugins/uploadKit.bat
+++ FinalWorldBuildingProject/Assets/Plugins/uploadKit.bat
@@ -1,1 +1,1 @@
-curl -v -b cookie -X PUT -F "kit[zip]=@/Users/thomaspiccolo/Documents/GitHub/FinalWorldBuildingProject/FinalWorldBuildingProject/1637838345809691190_checkpressedupload.zip" -F "kit[game_engine_version]=20194" https://account.altvr.com/api/kits/1637838345809691190.json
+curl -v -b cookie -X PUT -F "kit[zip]=@/Users/thomaspiccolo/Documents/GitHub/FinalWorldBuildingProject/FinalWorldBuildingProject/1638473701680743338_checkedpressedupload1.zip" -F "kit[game_engine_version]=20194" https://account.altvr.com/api/kits/1638473701680743338.json
